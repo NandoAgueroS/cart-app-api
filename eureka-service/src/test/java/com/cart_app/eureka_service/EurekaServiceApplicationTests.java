7b67da8dab93698bf0bf75b95eec94512b35b579
NandoAgueroS <nandoaguerojs@gmail.com>
2024-12-26 21:03:47 -0300

    First commit

---
+++ eureka-service/src/test/java/com/cart_app/eureka_service/EurekaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cart_app.eureka_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
