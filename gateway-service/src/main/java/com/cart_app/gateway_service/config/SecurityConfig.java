4b87d2e5bc49ec768e243cb752a0bffbbfaac950
NandoAgueroS <nandoaguerojs@gmail.com>
2024-12-29 17:21:02 -0300

    Add Spring Security and Keycloak to API Gateway

---
+++ gateway-service/src/main/java/com/cart_app/gateway_service/config/SecurityConfig.java
@@ -0,0 +1,28 @@
+package com.cart_app.gateway_service.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Autowired
+    private JwtAuthenticationConverter jwtAuthenticationConverter;
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity httpSecurity) {
+        return httpSecurity
+                .csrf(csrf -> csrf.disable())
+                .authorizeExchange(http -> http.
+                        pathMatchers(HttpMethod.POST,"/api/users").permitAll()
+                        .pathMatchers(HttpMethod.GET, "/api/users").hasRole("admin_client_role")
+                        .anyExchange().authenticated())
+                .oauth2ResourceServer(oauth ->
+                        oauth.jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter)))
+                .build();
+    }
+}
