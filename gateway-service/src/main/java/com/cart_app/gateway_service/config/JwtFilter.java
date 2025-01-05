6fae2470a998bfc5cdb243bacd148aecb242a70c
NandoAgueroS <nandoaguerojs@gmail.com>
2025-01-04 21:29:14 -0300

    Add user ID to headers in API Gateway

---
+++ gateway-service/src/main/java/com/cart_app/gateway_service/config/JwtFilter.java
@@ -0,0 +1,45 @@
+package com.cart_app.gateway_service.config;
+
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.ReactiveSecurityContextHolder;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.WebFilter;
+import org.springframework.web.server.WebFilterChain;
+import reactor.core.publisher.Mono;
+
+@Component
+public class JwtFilter implements WebFilter {
+
+    private Mono<String> getUserId(){
+        return ReactiveSecurityContextHolder.getContext().map(securityContext -> {
+            Authentication authentication = securityContext.getAuthentication();
+            if (authentication != null && authentication.getPrincipal() instanceof Jwt){
+                Jwt jwt = (Jwt) authentication.getPrincipal();
+                String userId = jwt.getClaimAsString("sub");
+                return userId;
+            }
+            System.out.println("failed to get user id");
+            return null;
+        });
+    }
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
+        return getUserId().flatMap(userId -> {
+            if (userId != null){
+                ServerHttpRequest modifiedRequest = exchange.getRequest()
+                        .mutate()
+                        .header("User-Id-Personalized", userId)
+                        .build();
+                ServerWebExchange modifiedExchange = exchange.mutate().request(modifiedRequest).build();
+                return chain.filter(modifiedExchange);
+            }
+            return chain.filter(exchange);
+        });
+    }
+
+
+}
--- gateway-service/src/main/java/com/cart_app/gateway_service/config/SecurityConfig.java
+++ gateway-service/src/main/java/com/cart_app/gateway_service/config/SecurityConfig.java
@@ -4,9 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
+@EnableWebFluxSecurity
 @Configuration
 public class SecurityConfig {
 
